9d6edae46f41a41650edf561f55bb8a41327c720
Nuno Fernandes <fernandes.n.sa@outlook.fr>
2022-09-18 15:13:48 +0200

    correct navbar toogler in mobile version

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,12 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     var elems = document.querySelectorAll(".sidenav");
-//     var instances = M.Sidenav.init(elems);
-// });
+let divCollapse = document.getElementById("navbarSupportedContent");
+let classes = divCollapse.classList;
+let liNav = document.querySelectorAll(".nav-item");
+let btn = document.getElementById("btnToogler");
+
+for (let i = 0; i < liNav.length; i++) {
+    liNav[i].addEventListener("click", () => {
+        classes.remove("show");
+        btn.classList.add("collapsed");
+        btn.setAttribute("aria-expanded", false);
+    });
+}
